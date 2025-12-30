7c2dacbb32ba2b699a8c1a4e191a912f72039f33
Yoonchulchung <yoonchul005@gmail.com>
2025-12-31 00:37:13 +0900

    feat(#0): Intialize Spring Boot

---
+++ src/test/java/com/example/RealMatch/RealMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
