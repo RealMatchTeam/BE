d6d96b29d0c3187df2004b07868eeebba6434793
Yoonchulchung <yoonchul005@gmail.com>
2026-01-22 00:32:36 +0900

    fix(#34): import 규칙 해결

--- src/main/java/com/example/RealMatch/campaign/domain/entity/CampaignLikeEntity.java
+++ src/main/java/com/example/RealMatch/campaign/domain/entity/CampaignLikeEntity.java
@@ -1,7 +1,5 @@
 package com.example.RealMatch.campaign.domain.entity;
 
-import java.time.LocalDateTime;
-
 import com.example.RealMatch.global.common.BaseEntity;
 import com.example.RealMatch.user.domain.entity.UserEntity;
 
