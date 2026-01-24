4e45bd6647da7706f9f84a3bfe0f878e00c31375
yerimi00 <fhddl1019@naver.com>
2026-01-24 17:13:51 +0900

    refactor(exception): 예외 처리 메커니즘 표준화

--- src/main/java/com/example/RealMatch/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/RealMatch/global/exception/handler/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.example.RealMatch.global.exception.handler;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import com.example.RealMatch.global.exception.GeneralException;
 import com.example.RealMatch.global.response.ErrorResponse;
-// @RestControllerAdvice // 빈 충돌을 막기 위해 비활성화
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(GeneralException.class)
--- src/main/java/com/example/RealMatch/global/presentation/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/RealMatch/global/presentation/advice/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestControllerAdvice
+// @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
