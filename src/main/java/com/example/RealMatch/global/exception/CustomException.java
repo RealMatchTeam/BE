4beb7709187b1ffd383c8c6d2ef3788896394058
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-01-28 03:36:22 +0900

    feat: 공통 에러 처리 customException 추가

---
+++ src/main/java/com/example/RealMatch/global/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.example.RealMatch.global.exception;
+
+import com.example.RealMatch.global.presentation.code.BaseErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+
+    private final BaseErrorCode code;
+
+    public CustomException(BaseErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode;
+    }
+}
