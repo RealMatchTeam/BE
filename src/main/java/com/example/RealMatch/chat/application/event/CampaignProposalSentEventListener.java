62a320ae099a2d2487371fac6ecbf5da36f9eda6
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-02-04 18:33:09 +0900

    chore: expired를 Cancel로 변경

--- src/main/java/com/example/RealMatch/chat/application/event/CampaignProposalSentEventListener.java
+++ src/main/java/com/example/RealMatch/chat/application/event/CampaignProposalSentEventListener.java
@@ -80,7 +80,7 @@ public class CampaignProposalSentEventListener {
             return ChatProposalDecisionStatus.PENDING;
         }
         return switch (proposalStatus) {
-            case CANCELED ->  ChatProposalDecisionStatus.EXPIRED;
+            case CANCELED ->  ChatProposalDecisionStatus.CANCELED;
             case REVIEWING, NONE -> ChatProposalDecisionStatus.PENDING;
             case MATCHED -> ChatProposalDecisionStatus.ACCEPTED;
             case REJECTED -> ChatProposalDecisionStatus.REJECTED;
