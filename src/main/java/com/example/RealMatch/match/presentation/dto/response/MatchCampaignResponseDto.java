30899d3f818b212b36120f375efcab78d7bbf6ea
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-02-06 18:37:58 +0900

    refactor: 매칭 캠페인 조회에서 campaignIsLike 응답 추가

--- src/main/java/com/example/RealMatch/match/presentation/dto/response/MatchCampaignResponseDto.java
+++ src/main/java/com/example/RealMatch/match/presentation/dto/response/MatchCampaignResponseDto.java
@@ -30,6 +30,7 @@ public class MatchCampaignResponseDto {
         private Integer campaignManuscriptFee;
         private String campaignName;   // 캠페인명 (keyword 검색 대상)
         private Integer campaignDDay;
+        private Boolean campaignIsLiked;
         private Integer campaignTotalRecruit;
         private Integer campaignTotalCurrentRecruit;
     }
